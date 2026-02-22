d055b0a74155880463132f6a6b4ad772372a5111
douglordofcoffee <madnoanddoug@gmail.com>
2026-02-22 14:03:05 -0600

    updated to sdk usage and msys2 on C:\

--- build.bat
+++ build.bat
@@ -1,9 +1,25 @@
 @echo off
-D:\mysys2\mingw64\bin\g++.exe -std=c++17 main.cpp Player.cpp Camera.cpp Layer.cpp Chunk.cpp TileMap.cpp NoiseGenerator.cpp TextureManager.cpp TextRenderer.cpp WorldLoader.cpp WorldImporter.cpp Minimap.cpp Menu.cpp -o game -LD:\SDL2-2.32.6\x86_64-w64-mingw32\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -ID:\SDL2-2.32.6\x86_64-w64-mingw32\include -IC:\msys64\mingw64\include -IC:\msys64\mingw64\include\SDL2 -LD:\SDL2-2.32.6\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf 
-if %errorlevel% equ 0 (
-    echo Compilation successful! 
-    game.exe
-) else (
-    echo Compilation failed!
+:: --- PATH CONFIGURATION ---
+SET SDK_PATH=C:/SDK
+SET SDL_CORE=%SDK_PATH%/SDL2-2.32.6/x86_64-w64-mingw32
+SET SDL_IMG=%SDK_PATH%/SDL2_image-2.0.5/x86_64-w64-mingw32
+SET SDL_TTF=%SDK_PATH%/SDL2_ttf-2.22.0/x86_64-w64-mingw32
+
+:: --- COMPILATION ---
+echo Building Midgard...
+
+g++ -std=c++17 *.cpp -o game.exe ^
+-I"%SDL_CORE%/include" -I"%SDL_IMG%/include" -I"%SDL_TTF%/include" ^
+-L"%SDL_CORE%/lib" -L"%SDL_IMG%/lib" -L"%SDL_TTF%/lib" ^
+-lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf ^
+-mwindows
+
+if %errorlevel% neq 0 (
+    echo.
+    echo [!] Compilation failed! Check the errors above.
     pause
+    exit /b %errorlevel%
 )
+
+echo [!] Build Successful! Starting game...
+start game.exe
